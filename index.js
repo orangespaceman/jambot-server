4a08b8f4c52847a784eac5a0a4e4854aa48c55ae
Pete Goodman <thegingerbloke@users.noreply.github.com>
2018-04-05 12:11:51 +0100

    Add jambot state

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
+var jambotConnected = false;
+var jambotSocket = null;
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
@@ -10,11 +13,26 @@ app.get("/", function(req, res) {
 app.use(express.static("public"));
 
 io.on("connection", function(socket) {
+  socket.on("client", function() {
+    socket.emit("jambotConnected", jambotConnected);
+  });
+  socket.on("jambot", function() {
+    jambotConnected = true;
+    jambotSocket = socket;
+    socket.broadcast.emit("jambotConnected", jambotConnected);
+  });
   socket.on("play", function(msg) {
     io.emit("play", msg);
   });
   socket.on("say", function(msg) {
     io.emit("say", msg);
+  });
+  socket.on("disconnect", function() {
+    if (socket === jambotSocket) {
+      jambotConnected = false;
+      jambotSocket = null;
+      socket.broadcast.emit("jambotConnected", jambotConnected);
+    }
   });
 });
 
--- public/script.js
+++ public/script.js
@@ -11,9 +11,27 @@ $(".play-link").click(e => {
   socket.emit("play", e.target.textContent);
 });
 
+socket.emit("client");
+
 socket.on("play", msg => log("play", msg));
 
 socket.on("say", msg => log("say", msg));
+
+socket.on("jambotConnected", jambotConnected => {
+  updateState(jambotConnected);
+  if (jambotConnected) {
+    log("JamBot connected", "Hooray!");
+  } else {
+    log("JamBot disconnected", "Boo!");
+  }
+});
+
+function updateState(jambotConnected) {
+  var $value = $(".jambot-state-value");
+  $value.fadeOut(function(){
+    $value.text(jambotConnected ? 'online' : 'offline').fadeIn();
+  });
+}
 
 function log(type, msg) {
   var date = new Date();
